d80082015f255d44693a90a3cee034f59a599535
unknown <mykhol1995@gmail.com>
2016-07-10 12:35:39 +0300

    Creating login page and controller

---
+++ src/main/java/web/common/LoginController.java
@@ -0,0 +1,13 @@
+package web.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String LoginPage() {
+        return "login";
+    }
+}
